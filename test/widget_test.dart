8034fb195d1bde27c1b2b0dbef028a8a4f3cb421
Ajkaushik9045 <staraj89@gmail.com>
2024-07-25 13:20:46 +0530

    final

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import '../Frontend/lib/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
